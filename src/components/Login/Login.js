b17e1922362190baeae5616c5e2b4eaa25f5fdfc
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-10-10 14:40:20 +0600

    added AuthContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Login.css'
 
 const Login = () => {
+    const {user, signInUsingGoogle} = useFirebase();
     return (
         <div className="login-form">
             <div>
@@ -19,7 +21,7 @@ const Login = () => {
 
                 </p>
                 <div>----------------or--------------</div>
-                <button className="btn-regular">Google Sign In</button>
+                <button className="btn-regular" onClick={signInUsingGoogle}>Google Sign In</button>
             </div>
         </div>
     );
